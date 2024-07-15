6ecfb5078b99a22a92d37930be64182e1bf7fc7f
LeonardoReichert <leoreichert5000@gmail.com>
2024-07-15 14:55:10 -0300

    imperativa, varias pruebas de declaracion y asignacion

--- programacion_imperativa/OrganizacionCodigo.java
+++ programacion_imperativa/OrganizacionCodigo.java
@@ -3,6 +3,26 @@ package programacion_imperativa;
 public class OrganizacionCodigo{
     public static void main(String[] args){
         System.out.println("hola java");
+
+
+        // declaracion
+        int entero1;
+
+        // asignacion
+        entero1 = 50;
+
+        //varias declaracioneas
+        //int entero2, entero3;
+
+        //varias declaaciones y asignaciones:
+        int entero2 = 2, entero3 = 3;
+        //int entero2,entero3 = 2; //esto no asigna entero2
+
+        //varias asignaciones con un mismo valor:
+        entero2 = entero3 = 4;
+        
+        System.out.println("entero1 vale " + entero1);
+        System.out.println("entero2 vale " + entero2 + " entero3 vale " + entero3);
     }
 }
 
